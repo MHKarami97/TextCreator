66275cc0d2b57e610a009edb96010d4ec5600d95
Mohammad Hossein Karami <mhkarami97@gmail.com>
2022-02-19 10:25:40 +0330

    update code

--- TextCreator/Program.cs
+++ TextCreator/Program.cs
@@ -5,7 +5,7 @@ try
     const int last = 111;
     const string resultFolder = "result";
     const string template = "<img src=\"/assets/images/39-qeshm/";
-    const string template2 = ".jpeg\" width=\"600\" height=\"600\" alt=\"mhkarami97\" />";
+    const string template2 = ".jpg\" width=\"600\" height=\"600\" alt=\"mhkarami97\" />";
 
     Directory.CreateDirectory(resultFolder);
     using var sw = new StreamWriter($"{resultFolder}.md");
