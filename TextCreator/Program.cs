1c088a53cae3a8192cb8050398c77f364d77be9d
Mohammad Hossein Karami <mhkarami97@gmail.com>
2022-02-19 10:24:24 +0330

    Initial commit

---
+++ TextCreator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Start");
+
+try
+{
+    const int last = 111;
+    const string resultFolder = "result";
+    const string template = "  <img src=\"/assets/images/39-qeshm/";
+    const string template2 = ".jpeg\" width=\"600\" height=\"600\" alt=\"mhkarami97\" />";
+
+    Directory.CreateDirectory(resultFolder);
+    using var sw = new StreamWriter($"{resultFolder}.md");
+
+    for (var i = 1; i < last; i++)
+    {
+        try
+        {
+            sw.WriteLine(template + i.ToString("000") + template2);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    Console.WriteLine("finish");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
